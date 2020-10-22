9325bdb952f40f1f845a01846438311ad74b0038
ashis.mohanty <rakesh.tomar@talentedge.in>
2020-10-22 15:42:08 +0530

    added codeigniter project

---
+++ application/controllers/User.php
@@ -0,0 +1,66 @@
+<?php
+class User extends CI_Controller{
+	function __construct()
+	{
+	 parent::__construct();
+	$this->load->model('Usermgt');
+	}
+	
+	
+	
+	function index(){
+		$this->load->view("user");
+	}
+
+	function userform(){
+		$data['universityList'] = $this->Usermgt->universitylist();
+		$data['userlist'] = $this->Usermgt->userlist();
+		$this->load->view("userindex",$data);
+	}
+	
+	function createuser(){
+		echo "<pre>";print_r($_REQUEST);die("Okk");
+		 $data = $_POST;
+		// $data['first_name']= $data['firstName'];
+		// $data['last_name']= $data['lastName'];
+		// $data['user_email']= $data['user_email'];
+		// $data['user_name']= $data['userName'];
+		// $data['password']= $data['password'];
+
+		// $data['gender']= $data['gender'];
+		// $data['regDate']= $data['dateOfBirth'];
+		// $data['phone_mobile']= $data['mobile'];
+		// $data['phone_work']= $data['mobileWork'];
+
+
+		// $data['regDate']= $data['street'];
+		// $data['regDate']= $data['address'];
+		// $data['regDate']= $data['pincode'];
+		// $data['address_country']= $data['country'];
+		// $data['address_state']= $data['state'];
+
+
+
+		//$data['dateEntered']= date("Y-m-d h:i:s";
+		
+
+
+
+
+		//echo "<pre>";print_r($data);die("Okk");
+		$res = $this->Usermgt->createuser($data);
+		$operation='';
+		if($res==true)
+		{
+			//$alldata=$this->echouserData("");
+			$operation=array("success"=>true,"data"=>"success","error"=>"No Error");
+			echo json_encode($operation);
+		}
+		else
+		{
+			$operation=array("success"=>false,"data"=>"","error"=>"User Creation Failed");
+			echo json_encode($operation);
+		}
+	}
+}
+?>
---
+++ application/models/Usermgt.php
@@ -0,0 +1,51 @@
+<?php
+class Usermgt  extends CI_Model{
+			
+		private $conn='';
+		 
+		function __construct(){
+			$this->load->database();
+		} 
+		
+		function __destruct() 
+		{        
+		}  
+
+		
+	public function createuser($data){
+		//die("okkk");
+		// $data['createdBy'] = "123"
+		// $data['createdOn'] = date("Y/m/d");
+		// $data['userType'] = "user";
+		//echo "<pre>";print_r($data);die("Okk");
+		if(!$this->db->insert('user', $data))
+		{
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	}
+        
+	public function updateuser($userid,$data){
+		$this->db->where('userId',$userid);  
+		if(!$this->db->update('users', $data)){
+			return false;
+		}else{
+			return true;
+		}
+	}
+
+	public function universitylist(){
+		$res = $this->db->query("SELECT * FROM `university` where status = 0 and deleated = 0");
+		return $res;
+	}
+
+	public function userlist(){
+		$res = $this->db->query("SELECT id,userName FROM `users` where status = 0");
+		return $res;
+	}
+
+}
+?>                                                                                                                                              
