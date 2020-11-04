b74be6f64004aec5c18a696d5c6b39c70ac86869
Sammaan Bansal <Sammaan@arrina.in>
2020-11-04 18:55:52 +0530

    program form added

--- application/views/user/createUserForm.php
+++ application/views/user/createUserForm.php
@@ -397,6 +397,63 @@
 	</article>
 </div>
 
+<div class="te-container">
+	<article class="form-style-1">
+    	<form name="createuserform" method="" enctype="multipart/form-data">
+    		<!-- <div class="form-group">
+	            	<label style="display: none;">User Photo <span class="required">*</span></label>
+            		<input type="file" id = "userPhoto" name="userPhoto" placeholder="description" />
+            		<figure class="upload-user-bg">
+            			<img src="assets/images/upload-user.png">
+            			<span>Upload Photo</span>
+            		</figure>
+            </div> -->
+
+            <div class="d-flex">
+	            <div class="form-group">
+	            	<input type="text" id = "programName" name="programName" />
+	            	<label class="floating-label">Program Name<span class="required">*</span></label>
+	            </div>
+	             <div class="form-group">    
+	            	<label>Program Status <span class="required">*</span></label>
+	                <select id = "programStatus" name="programStatus" class="field-divided">
+	                <option value="">Select Status</option>
+	                <option value="Active">Active</option>
+	                <option value="Inactive">Inactive</option>
+	              </select>
+	            </div>
+            </div>    
+
+            <div class="d-flex">
+            	<div class="form-group">
+	            	<input type="text" id = "createdBy" name="createdBy" />
+	            	<label class="floating-label">Created By<span class="required">*</span></label>
+	            </div>
+	            <div class="form-group">
+	                <input type="text" id = "modifiedBy" name="modifiedBy" />
+	                <label class="floating-label">Modified By<span class="required">*</span></label>
+	            </div>
+	        </div>
+
+			<div class="d-flex">
+	        	<div class="form-group">    
+	            	<label>Created Date <span class="required">*</span></label>
+	                <input type="date" id = "createdDate" name="createdDate"/>
+	            </div>
+	            <div class="form-group">    
+	            	<label>Modified Date <span class="required">*</span></label>
+	                <input type="date" id = "modifiedDate" name="modifiedDate"/>
+	            </div>
+            </div>
+
+
+            <div class="form-group cta-submit">
+                <input type="button" id ="createuserbtn" value="Submit" />
+            </div>
+        </form>
+	</article>
+</div>
+
 <script>
 $(document).on('click','#createuserbtn',function(e) {
 	e.preventDefault()
